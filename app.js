15ded525bf595bf2fa583d688ad8288032747596
Abdulaziz <111849967+abdulaziz200531@users.noreply.github.com>
2022-12-04 13:57:54 +0500

    finish

---
+++ app.js
@@ -0,0 +1,13 @@
+let person = [
+    Cars = 'BMW',
+    Cars1 = 'Volvo',
+    Cars2 = 'Saab',
+    Cars3 = 'Ford'
+];
+let i = 0;
+
+while(i != person.length){
+    console.log(person[i]);
+    i++;
+}
+
